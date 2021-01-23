30aac582aad3b6411517c66d199c8fed275ebbd5
wangsq <358009775@qq.com>
2021-01-23 23:32:13 +0800

    udpate entrypoint.sh，add crontab

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,26 +3,16 @@ set -e
 
 crond
 
-# cd /root/AutomationTTnode && git pull
+if [ -f /config/crontab.list ]
+then
+  echo -e "存在crontab.list，使用crontab.list添加定时任务"
+  crontab /config/crontab.list
+  echo -e "当前的定时任务如下：\n"
+  crontab -l
 
-# if [ -f /root/AutomationTTnode/crontab-custom.list ]
-# then
-#   echo -e "存在crontab-custom.list，使用crontab-custom.list添加定时任务"
-#   crontab /root/AutomationTTnode/crontab-custom.list
-# elif [ -f /root/AutomationTTnode/crontab.list ]
-# then
-#   echo -e "存在crontab.list，使用crontab.list添加定时任务"
-#   crontab /root/AutomationTTnode/crontab.list
-# fi
+else
+  echo -e "没有检测到crontab.list文件，暂无定时任务"
+  tail -f /dev/null
+fi
 
-echo -e "当前的定时任务如下：\n"
-# crontab -l
-# crond -L /root/crond.log
-# touch /root/crond.log
-
-# cd /root/AutomationTTnode
-
-# echo -e "定时任务执行中...\n"
-# tail -f /root/crond.log
-
-tail -f /dev/null
+exec "$@"
